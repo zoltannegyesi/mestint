dbf0d2430628b729bc9d79ec01abd932fce73bfd
Négyesi Zoltán <zoltannegyesi4@gmail.com>
2023-04-20 12:45:28 +0200

    Initial

---
+++ src/main/java/hu/nye/mestint/MestintApplication.java
@@ -0,0 +1,13 @@
+package hu.nye.mestint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MestintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MestintApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/nye/mestint/MestintApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.nye.mestint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MestintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
